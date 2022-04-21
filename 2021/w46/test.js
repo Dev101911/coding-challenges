d6b5a748809eda15c18df1498d05c39792438ec0
Devin Harmse <devinharmse@gmail.com>
2022-04-21 07:45:51 +0200

    w14 work

--- 2021/w46/test.js
+++ 2021/w46/test.js
@@ -6,8 +6,8 @@ const solutions = fs.readdirSync(path.resolve(__dirname, "solutions"));
 describe.each(solutions)("%s", (filename) => {
   const evaluate = require(path.resolve(__dirname, "solutions", filename));
   test.each([
-    [['abcd1abfafa5fafafa3rc'], 5],
-    [['qwerty17gga6fafa13ppp'], 17],
+    [["abcd1abfafa5fafafa3rc"], 5],
+    [["qwerty17gga6fafa13ppp"], 17],
   ])("%j = %j", (inputs, result) => {
     expect(evaluate(...inputs)).toStrictEqual(result);
   });
--- 2022/w14/solutions/solution.js
+++ 2022/w14/solutions/solution.js
@@ -1,7 +1,24 @@
+// North - South - East - West
+const directionMap = new Map([
+  ["NORTH", 1],
+  ["SOUTH", -1],
+  ["EAST", 2],
+  ["WEST", -2],
+]);
 const cutToTheChase = (directions) => {
-  // your solution here
-
-  return undefined;
+  const newArray = [];
+  for (let i = 0; i < directions.length; i++) {
+    if (
+      directionMap.get(newArray[newArray.length - 1]) +
+        directionMap.get(directions[i]) !==
+      0
+    ) {
+      newArray.push(directions[i]);
+    } else {
+      newArray.pop();
+    }
+  }
+  return newArray;
 };
 
 module.exports = cutToTheChase;
